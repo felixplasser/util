abb4185a70bd9ea03e23dc0cc9da7aceefc63cd8
Felix Plasser (lin) <f.plasser@lboro.ac.uk>
2025-07-04 15:58:03 +0100

    clean up more files

--- bin/clean_calcs.bash
+++ bin/clean_calcs.bash
@@ -2,9 +2,15 @@
 
 echo "$0 - Clean scratch files from computations"
 
+# General
+rmi -name 'core.[0-9]*'
+
+# Q-Chem
+rmi -name 'qarchive.h5'
+
 # OpenMolcas
-rmi -name '*Orb.?'
-h5r
+rmi -name '*Orb.[1-9]*'
+rmi -name '*.ChVec?'
 
 # Orca
 rmi -name '*.gbw'
@@ -13,3 +19,9 @@ rmi -name '*.densities'
 # Turbomole
 rmi -name 'CC*--*'
 rmi -name '*.cao'
+
+# Compress any HDF5 files
+#h5r
+
+echo "*** The following big files (+100M) are left ***"
+find . -size +100M
